3354783ce58a8779d41f062a728eae40f49eb7c9
aquraish716 <aquraish@gmail.com>
2019-09-11 20:13:26 -0500

    changed the second color to purple

---
+++ 01week/hello.js
@@ -0,0 +1,9 @@
+console.log('Hello Earth and Mars');
+
+const firstColor = 'blue';
+
+console.log('My first color is', firstColor);
+
+const secondColor = 'purple';
+
+console.log('My second color is', secondColor);
