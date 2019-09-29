e6f0cee47fc46547ed4b057d5628b0142602bc3e
aquraish716 <aquraish@gmail.com>
2019-09-29 11:21:54 -0500

    started the HTML and CSS for ticTacToe UI

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -25,22 +25,46 @@ function printBoard() {
 
 function horizontalWin() {
   // Your code here
+  for (let i = 0; i < board.length; i++) {
+    if(board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] == playerTurn) {
+      return true;
+    }
+  } return false;
 }
 
 function verticalWin() {
   // Your code here
+  for (let i = 0; i < board[0].length; i++) {
+    if (board[0][i] == playerTurn && board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+      return true;
+    }
+  } return false;
 }
+
 
 function diagonalWin() {
   // Your code here
+  if (board[1][1] == playerTurn) {
+    if (board[0][0] == playerTurn && board [2][2] == playerTurn) {
+      return true;
+    } else if (board[0][2] == playerTurn && board[2][0] == playerTurn) {
+      return true;
+    }
+  } else 
+  return false;
 }
 
 function checkForWin() {
   // Your code here
+  horizontalWin();
+  verticalWin();
+  diagonalWin();
+  return true;
 }
 
 function ticTacToe(row, column) {
   // Your code here
+  
 }
 
 function getPrompt() {
