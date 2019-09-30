bc60d3ca6ad3e906c1a487068a8192da6a90b77e
aquraish716 <aquraish@gmail.com>
2019-09-29 21:03:51 -0500

    Created a cdd and html for the tictactoe js and made it update the boxes with either x or o by using get element by id and then using innerHTML to update it with what was clicked.I also added popup messages to let player know if something is already in the box and also to let someone know once they have one. Once there is a winner, I added the reload function on the JS to refresh the page and start new.

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -1,11 +1,5 @@
 'use strict';
 
-const assert = require('assert');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 let board = [
   [' ', ' ', ' '],
   [' ', ' ', ' '],
@@ -22,40 +16,38 @@ function printBoard() {
   console.log('  ---------');
   console.log('2 ' + board[2].join(' | '));
 }
-
 function horizontalWin() {
-  // Your code here
   for (let i = 0; i < board.length; i++) {
     if(board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] == playerTurn) {
-      return true;
+      window.alert("You are the winner!!!");
+      document.location.reload(true);
     }
   } return false;
 }
 
 function verticalWin() {
-  // Your code here
   for (let i = 0; i < board[0].length; i++) {
     if (board[0][i] == playerTurn && board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
-      return true;
+      window.alert("You are the winner!!!");
+      document.location.reload(true);
     }
   } return false;
 }
 
-
 function diagonalWin() {
-  // Your code here
   if (board[1][1] == playerTurn) {
     if (board[0][0] == playerTurn && board [2][2] == playerTurn) {
-      return true;
+      window.alert("You are the winner!!!");
+      document.location.reload(true);
     } else if (board[0][2] == playerTurn && board[2][0] == playerTurn) {
-      return true;
+      window.alert("You are the winner!!!");
+      document.location.reload(true);
     }
   } else 
   return false;
 }
 
 function checkForWin() {
-  // Your code here
   horizontalWin();
   verticalWin();
   diagonalWin();
@@ -63,55 +55,26 @@ function checkForWin() {
 }
 
 function ticTacToe(row, column) {
-  // Your code here
+  if (board[row][column] === ' ') {
+    board[row][column] = playerTurn;
+  } else {
+    console.log('Please try again, place has been taken.');
+    window.alert('Please try again, place has been taken.');
+
+    return playerTurn;
+  }
+  checkForWin();
   
-}
-
-function getPrompt() {
-  printBoard();
-  console.log("It's Player " + playerTurn + "'s turn.");
-  rl.question('row: ', (row) => {
-    rl.question('column: ', (column) => {
-      ticTacToe(row, column);
-      getPrompt();
-    });
-  });
-
-}
-
-
-
-// Tests
-
-if (typeof describe === 'function') {
-
-  describe('#ticTacToe()', () => {
-    it('should place mark on the board', () => {
-      ticTacToe(1, 1);
-      assert.deepEqual(board, [ [' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
-    });
-    it('should alternate between players', () => {
-      ticTacToe(0, 0);
-      assert.deepEqual(board, [ ['O', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
-    });
-    it('should check for vertical wins', () => {
-      board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
-      assert.equal(verticalWin(), true);
-    });
-    it('should check for horizontal wins', () => {
-      board = [ ['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' '] ];
-      assert.equal(horizontalWin(), true);
-    });
-    it('should check for diagonal wins', () => {
-      board = [ ['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X'] ];
-      assert.equal(diagonalWin(), true);
-    });
-    it('should detect a win', () => {
-      assert.equal(checkForWin(), true);
-    });
-  });
-} else {
-
-  getPrompt();
-
-}
+  if (playerTurn == 'X') {
+    playerTurn = 'O';
+    let playerX = document.getElementById('box'+row+column);
+    playerX.innerHTML = 'X';
+    checkForWin();
+  }
+  else if (playerTurn == 'O') {
+    playerTurn = 'X';
+    let playerO = document.getElementById('box'+row+column);
+    playerO.innerHTML = 'O';
+    checkForWin();
+  }
+} 
