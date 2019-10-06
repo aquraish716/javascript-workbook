455aca9003eeb171a4ffd3ac8ca00e30bd57d485
aquraish716 <aquraish@gmail.com>
2019-10-05 21:10:43 -0500

    created generateHint and mastermind functions in order to have the game run and all tests passing

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -28,15 +28,59 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
-  // your code here
+function generateHint(guess) {
+
+  //created variable solutionArray that is the split of string solution  
+  let solutionArray = solution.split("");
+  //created variable guessArray that is the split of string guess
+  let guessArray = guess.split("");
+  //created variable for the hint of correct letter location
+  let correctLetterLocations = 0;
+  //created variable for the hint of amount of correct letters
+  let correctLetters = 0;
+
+  /*created for loop to go through each i of solutionArray and guessArray and add 1 if i matchs on 
+  both to get the amount of correct letter locations*/
+  for(let i = 0; i < 4; i++){
+    if(solutionArray[i] === guessArray[i]){
+      correctLetterLocations += 1;
+      solutionArray[i] = null;
+    }
+  }
+  /*created for loop to go through each i of solutionArray and guessArray and add 1 if i matchs on 
+  both to get the amount of correct letters*/
+  for(let i = 0; i < 4; i++){
+    let targetIndex = solutionArray.indexOf(guessArray[i])
+    if(targetIndex > -1){
+      correctLetters += 1;
+      solutionArray[targetIndex] = null;
+    }
+  }
+//ends funtion by returning the amount of correctletterlocations and correct letters
+return correctLetterLocations+"-"+correctLetters;
 }
 
 function mastermind(guess) {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
-}
+  //defined a global variable
+  const solution = 'abcd';
 
+  //if statement to return "You guessed it!" if the guess equals the solution
+  if(guess === solution){
+    console.log("You guessed it!");
+    return "You guessed it!";
+  } 
+  //Provides hint if guess does not equal solution
+  else if (guess != solution){
+    let hint = generateHint(guess);
+    board.push(guess+"-"+hint);
+  }
+  //created if statement that lets you know to guess again or let you know that you have used up 10 guesses
+  if(board.length == 10){
+    console.log('You ran out of turns! The solution was ' + solution);
+  }else{
+    console.log('Guess again.');
+  }
+}
 
 function getPrompt() {
   rl.question('guess: ', (guess) => {
@@ -45,7 +89,6 @@ function getPrompt() {
     getPrompt();
   });
 }
-
 // Tests
 
 if (typeof describe === 'function') {
